4d8c3bc09dbf75485931486244102fefdc240242
alwinaind <alwinaind@gmail.com>
2018-01-16 11:09:59 +0530

    updated settings for heroku

--- mnist/settings.py
+++ mnist/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@1g%y(f$012g@+(h9%80lh@(=h=i!8bp-6(*ge-8@jbb=^z94v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['aqueous-earth-35734.herokuapp.com/']
 
