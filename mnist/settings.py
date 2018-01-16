0e5c6f06019e755eef16c830c273cc8cbabca799
alwinaind <alwinaind@gmail.com>
2018-01-16 11:16:31 +0530

    "heroku config"

--- mnist/settings.py
+++ mnist/settings.py
@@ -120,8 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_ROOT=os.path.join(BASE_DIR, "/static/")
 STATIC_URL = '/static/'
-STATIC_ROOT= "/static/"
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
